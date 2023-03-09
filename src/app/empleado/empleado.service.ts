3af981f921336811ad4054a4d6643a0793a68271
Frank <francisco.graya26@gmail.com>
2023-03-09 11:33:30 -0600

    se reflejan los datos en el front del back

--- src/app/empleado/empleado.service.ts
+++ src/app/empleado/empleado.service.ts
@@ -1,11 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Empleado } from '../empleado';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  //api a consumir de empleado
+  private baseUrl = "http://localhost:4000/api/empleado";
+  constructor(private httpCliente : HttpClient) { }
 
-  private baseUrl = ""
+
+  //obtener los empleados
+  obtenerListaDeEmpleados():Observable<Empleado[]>{
+    return this.httpCliente.get<Empleado[]>(`${this.baseUrl}`);
+  }
+
 }
